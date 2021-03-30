ef8fab536b734e485cb0304106f264aa4da9d4ed
Leo Gallucci <leo.gallucci@zalando.de>
2021-03-30 21:42:47 +0200

    Fix 2 fans on Zotac RTX 3090

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='coolgpus',
-    version='0.23',
+    version='0.23.0.1',
     description='GPU fan control for headless Linux',
-    url='https://github.com/andyljones/coolgpus',
-    author='Andy L Jones',
+    url='https://github.com/elgalu/coolgpus',
+    author=['Andy L Jones', 'Leo Gallucci'],
     author_email='andyjones.ed@gmail.com',
     license='MIT',
     scripts=['coolgpus']
